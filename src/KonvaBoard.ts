670c87575a526eaf9376892aae29f86638321d0b
keisokoo <keisokoo@gmail.com>
2023-12-09 15:53:49 +0900

    feat, konva 로 드로잉 구현, 브러쉬, 삭제 기능, undo, redo

--- src/KonvaBoard.ts
+++ src/KonvaBoard.ts
@@ -1,10 +1,10 @@
 import Konva from "konva";
 import generateHash from "./helper/generateHash";
 
-type ModeType = 'brush' | 'eraser' | 'erase-line';
+type ModeType = 'brush' | 'eraser' | 'delete';
 class BrushOptions {
-  brushSize: number = 55;
-  color: string = '#ffffff';
+  brushSize: number = 5;
+  color: string = '#df4b26';
   constructor(options?: Partial<BrushOptions>) {
     if (options) {
       this.brushSize = options.brushSize || this.brushSize;
@@ -24,9 +24,29 @@ class BrushOptions {
     }
   }
 }
+type HistoryStack = {
+  id: string,
+  lines: Konva.Line[],
+  mode: ModeType,
+  action: 'add' | 'remove'
+}
+const eraseLineDefault = {
+  brushSize: 5,
+  color: '#ffffff'
+} as const;
+type CallbackData = {
+  message: string,
+  data: {
+    mode: ModeType,
+    brushSize: number,
+    color: string
+    undoStack: HistoryStack[],
+    redoStack: HistoryStack[]
+  }
+}
 class KonvaBoard {
   el: HTMLDivElement;
-  cursorCanvas: HTMLCanvasElement;
+  cursor: HTMLDivElement;
   private width: number;
   private height: number;
   private stage: Konva.Stage;
@@ -34,32 +54,29 @@ class KonvaBoard {
   private isPaint: boolean;
   private mode: ModeType = 'brush';
   private isEraseLine: boolean = false;
+  private undoStack: HistoryStack[] = [];
+  private redoStack: HistoryStack[] = [];
+  private lastRemovedLines: Set<Konva.Line> = new Set();
   brushes: {
     brush: BrushOptions,
     eraser: BrushOptions,
-    'erase-line': BrushOptions
+    'delete': BrushOptions
   } = {
       brush: new BrushOptions(),
       eraser: new BrushOptions({
         brushSize: 10,
         color: '#ffffff'
       }),
-      'erase-line': new BrushOptions({
-        brushSize: 5,
-        color: '#ffffff'
-      })
+      'delete': new BrushOptions(eraseLineDefault)
     }
   currentBrush: BrushOptions = this.brushes[this.mode];
 
   private lastLine: Konva.Line = new Konva.Line();
   lines: Konva.Line[] = [];
 
-  cb: (data: string) => void;
+  cb: (data: CallbackData) => void;
 
-  constructor(el: HTMLDivElement, cb: (data: string) => void) {
-    this.cursorCanvas = document.querySelector('#cursor') ? document.querySelector('#cursor')! : document.createElement('canvas');
-    this.cursorCanvas.id = 'cursor';
-    this.cursorCanvas.style.display = 'none';
+  constructor(el: HTMLDivElement, cb: (data: CallbackData) => void) {
     this.cb = cb;
     this.el = el;
     this.width = window.innerWidth;
@@ -76,75 +93,163 @@ class KonvaBoard {
 
     this.isPaint = false;
 
+    if (document.querySelector('#cursor')) {
+      this.cursor = document.querySelector('#cursor')!;
+    } else {
+      this.cursor = document.createElement('div');
+      this.el.appendChild(this.cursor);
+    }
+    this.cursor.id = 'cursor';
+    this.cursor.style.display = 'none';
     this.init()
-    this.modeChange(this.mode)
+    this.setMode(this.mode)
   }
+  getCurrentData(message?: string): CallbackData {
+    return {
+      message: message ? message : 'done',
+      data: {
+        mode: this.mode,
+        brushSize: this.currentBrush.brushSize,
+        color: this.currentBrush.color,
+        undoStack: this.undoStack,
+        redoStack: this.redoStack
+      }
+    }
+  }
+  updated(message?: string) {
+    this.cb(this.getCurrentData(message));
+  }
+  appendStack(lines: Konva.Line[]) {
+    if (!lines || lines.length < 0) return;
+    const stack: HistoryStack = { id: `stack-${generateHash()}`, lines, mode: this.mode, action: this.mode === 'delete' ? 'remove' : 'add' }
 
+    this.undoStack.push(stack);
+    this.updated('appendStack');
+  }
+  undo() {
+    if (this.undoStack.length === 0) return;
+    let last = this.undoStack.pop();
+    if (!last) return;
+    if (last.lines.length === 0) return;
+    if (last.action === 'remove') {
+      last.lines.forEach(line => {
+        this.layer.add(line);
+      })
+      last.action = 'add';
+      this.redoStack.push(last);
+    } else {
+      last.lines.forEach(line => {
+        line.remove();
+      })
+      last.action = 'remove';
+      this.redoStack.push(last);
+    }
+    this.updated('undo');
+  }
+  redo() {
+    if (this.redoStack.length === 0) return;
+    let last = this.redoStack.pop();
+    if (!last) return;
+    if (last.lines.length === 0) return;
+    if (last.action === 'remove') {
+      last.lines.forEach(line => {
+        this.layer.add(line);
+      })
+      last.action = 'add';
+      this.undoStack.push(last);
+    } else {
+      last.lines.forEach(line => {
+        line.remove();
+      })
+      last.action = 'remove';
+      this.undoStack.push(last);
+    }
+    this.updated('redo');
+  }
+  private cursorStyle() {
+    this.cursor.style.position = 'fixed';
+    this.cursor.style.zIndex = '99999';
+    this.cursor.style.pointerEvents = 'none';
+    this.cursor.style.transform = 'translate(-50%, -50%)';
+    this.cursor.style.border = '1px solid #000000';
+  }
+  hideCursor() {
+    this.cursor.style.display = 'none';
+  }
   drawCursor(x: number, y: number) {
     if (!this.el) return;
     if (!this.currentBrush) return;
+    if (x < 0 || x > this.width || y < 0 || y > this.height) {
+      this.hideCursor();
+      return;
+    }
     const brushSize = this.currentBrush.brushSize;
-    const canvas = this.cursorCanvas;
-    canvas.width = brushSize;
-    canvas.height = brushSize;
-    this.cursorCanvas.style.display = 'block';
-    canvas.style.position = 'fixed';
-    canvas.style.zIndex = '99999';
-    canvas.style.pointerEvents = 'none';
-    canvas.style.top = `${y + this.el.offsetTop}px`;
-    canvas.style.left = `${x - this.el.offsetLeft}px`;
-    canvas.style.transform = 'translate(-50%, -50%)';
-    if (!canvas) return;
-    canvas.style.border = '1px solid #000000';
-    if (this.mode === 'eraser' || this.mode === 'erase-line') {
-      canvas.style.borderRadius = '0px';
+    this.cursor.style.width = brushSize + 'px';
+    this.cursor.style.height = brushSize + 'px';
+    this.cursor.style.display = 'block';
+    this.cursor.style.top = `${y + this.el.offsetTop}px`;
+    this.cursor.style.left = `${x - this.el.offsetLeft}px`;
+    if (!this.cursor) return;
+    if (this.mode === 'eraser' || this.mode === 'delete') {
+      this.cursor.style.borderRadius = '0px';
     } else {
-      canvas.style.borderRadius = '50%';
+      this.cursor.style.borderRadius = '50%';
     }
   }
 
   init() {
+    this.cursorStyle();
     this.stage.on('pointerdown', (e) => {
+      this.lastRemovedLines.clear();
       this.isPaint = true;
-      if (this.mode === 'erase-line') this.isEraseLine = true;
+      if (this.mode === 'delete') {
+        this.isEraseLine = true;
+        return
+      }
       const pos = this.stage.getPointerPosition();
       if (!pos) return;
       this.lastLine = new Konva.Line({
         id: `${this.mode}-${generateHash()}`,
-        stroke: '#df4b26',
+        stroke: this.brushes[this.mode].color,
         strokeWidth: this.brushes[this.mode].brushSize,
         globalCompositeOperation:
           this.mode === 'brush' ? 'source-over' : 'destination-out',
-        // round cap for smoother lines
-        lineCap: 'round',
-        lineJoin: 'round',
+        lineCap: this.mode === 'eraser' ? 'square' : 'round',
+        lineJoin: this.mode === 'eraser' ? 'miter' : 'round',
         hitStrokeWidth: this.brushes[this.mode].brushSize,
-        // add point twice, so we have some drawings even on a simple click
         points: [pos.x, pos.y, pos.x, pos.y],
       });
       this.layer.add(this.lastLine);
 
-      this.lastLine.on('mouseover', (e) => { // debug
+      this.lastLine.on('pointerover', (e) => {
         const id = e.target.id();
-        console.log('Lines', id, id.startsWith('brush-'), this.isEraseLine, this.isPaint)
-        if (this.mode === 'erase-line' && id.startsWith('brush-')) {
+        const line = this.lines.find(line => line.id() === id);
+        if (this.mode === 'delete' && id.startsWith('brush-') && this.isEraseLine && line) {
+          this.lastRemovedLines.add(line);
           e.target.remove()
         } else {
-          this.cb('Mouseover line, ' + id);
+          this.updated('pointerover');  // debug
         }
       });
-      this.lastLine.on('mouseout', () => { // debug
-        console.log('Lines', this.lines)
-        this.cb('Mouseout line');
+      this.lastLine.on('pointerout', () => {
+        this.updated('pointerout');  // debug
       });
       this.lines.push(this.lastLine);
+      this.updated('pointerdown');
     });
-    this.stage.on('pointerup', (e) => {
+    this.stage.on('pointerup', () => {
       this.isPaint = false;
       this.isEraseLine = false;
+      this.updated('pointerup');
+      this.appendStack(this.mode === 'delete' ? Array.from(this.lastRemovedLines) : [this.lastLine]);
     });
-
-    // and core function - drawing
+    this.el.addEventListener('pointerleave', () => {
+      this.isPaint = false;
+      this.isEraseLine = false;
+      this.hideCursor()
+    })
+    this.el.addEventListener('pointerenter', () => {
+    })
     this.stage.on('pointermove', (e) => {
       this.drawCursor(e.evt.offsetX, e.evt.offsetY);
       if (!this.isPaint) {
@@ -153,7 +258,6 @@ class KonvaBoard {
       if (this.isEraseLine) {
         return;
       }
-      // prevent scrolling on touch devices
       e.evt.preventDefault();
 
       const pos = this.stage.getPointerPosition();
@@ -162,10 +266,22 @@ class KonvaBoard {
       this.lastLine.points(newPoints);
     });
   }
-  modeChange(newMode: ModeType) {
+  setMode(newMode: ModeType) {
     this.mode = newMode;
     this.currentBrush = this.brushes[this.mode];
-    // this.createBrushCursor(this.currentBrush.brushSize);
+    this.updated('setMode');
+    return this.currentBrush.getBrushOptions();
+  }
+  setBrushSize(brushSize: number) {
+    if (this.mode === 'delete') return eraseLineDefault;
+    this.currentBrush.setBrushSize(brushSize);
+    this.updated('setBrushSize');
+    return this.currentBrush.getBrushOptions();
+  }
+  setColor(color: string) {
+    if (this.mode === 'delete') return eraseLineDefault;
+    this.currentBrush.setColor(color);
+    this.updated('setColor');
     return this.currentBrush.getBrushOptions();
   }
 }
--- stories/Konva.stories.tsx
+++ stories/Konva.stories.tsx
@@ -6,41 +6,104 @@ import './canvas.css'
 export const Demo = () => {
   const containerRef = React.useRef<HTMLDivElement>(null)
   const [methods, set_Methods] = React.useState<KonvaBoard | null>(null)
-  const [canvasData, set_canvasData] = React.useState<string>('')
+  const [description, set_description] = React.useState<string>('')
+  const [canvasData, set_canvasData] = React.useState<{
+    brushSize: number
+    color: string
+  }>({ brushSize: 1, color: '#000000' })
+  const [currentMode, set_currentMode] = React.useState<string>('brush')
+  const [stackLength, set_stackLength] = React.useState<{
+    undo: number
+    redo: number
+  }>({ undo: 0, redo: 0 })
   useEffect(() => {
     if (!containerRef.current) return
-    const konvaBoard = new KonvaBoard(containerRef.current, (data) => {
-      set_canvasData(data)
+    const konvaBoard = new KonvaBoard(containerRef.current, (values) => {
+      set_description(values.message)
+      set_stackLength({
+        undo: values.data.undoStack.length,
+        redo: values.data.redoStack.length,
+      })
     })
+    set_canvasData(konvaBoard.currentBrush.getBrushOptions())
     set_Methods(konvaBoard)
   }, [])
   return (
     <div className="canvas-wrap">
-      <canvas id="cursor" width="50" height="50"></canvas>
-      <div className="msg">{canvasData}</div>
+      <div className="msg">{description}</div>
       {methods && (
-        <div className="buttons">
-          <button
-            onClick={() => {
-              methods.modeChange('brush')
-            }}
-          >
-            brush
-          </button>
-          <button
-            onClick={() => {
-              methods.modeChange('eraser')
-            }}
-          >
-            eraser
-          </button>
-          <button
-            onClick={() => {
-              methods.modeChange('erase-line')
-            }}
-          >
-            erase-line
-          </button>
+        <div className="control-box">
+          <div className="buttons">
+            <button
+              disabled={stackLength.undo === 0}
+              onClick={() => {
+                methods.undo()
+              }}
+            >
+              undo ({stackLength.undo})
+            </button>
+            <button
+              disabled={stackLength.redo === 0}
+              onClick={() => {
+                methods.redo()
+              }}
+            >
+              redo ({stackLength.redo})
+            </button>
+            <button
+              onClick={() => {
+                set_canvasData(methods.setMode('brush'))
+                set_currentMode('brush')
+              }}
+              className={currentMode === 'brush' ? 'active' : ''}
+            >
+              brush
+            </button>
+            <button
+              onClick={() => {
+                set_canvasData(methods.setMode('eraser'))
+                set_currentMode('eraser')
+              }}
+              className={currentMode === 'eraser' ? 'active' : ''}
+            >
+              eraser
+            </button>
+            <button
+              onClick={() => {
+                set_canvasData(methods.setMode('delete'))
+                set_currentMode('delete')
+              }}
+              className={currentMode === 'delete' ? 'active' : ''}
+            >
+              delete
+            </button>
+            <div className="controls">
+              <input
+                disabled={currentMode === 'delete'}
+                type="color"
+                id="color"
+                name="color"
+                value={canvasData.color}
+                onChange={(e) => {
+                  const changed = methods.setColor(e.target.value)
+                  set_canvasData(changed)
+                }}
+              />
+            </div>
+            <div className="controls">
+              <input
+                disabled={currentMode === 'delete'}
+                type="number"
+                id="brushSize"
+                name="brushSize"
+                value={canvasData.brushSize}
+                onChange={(e) => {
+                  const changed = methods.setBrushSize(Number(e.target.value))
+                  set_canvasData(changed)
+                }}
+              />
+            </div>
+          </div>
         </div>
       )}
       <div ref={containerRef}></div>
